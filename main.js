e880d9a7fb7a16f423876f225e3750ca82771574
karim <kokekarim@yahoo.com>
2019-01-23 17:17:59 +0200

    test2

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 //BUDGET CONTROLLER
-var budgetController = (function() {})();
+var budgetController = (function() {
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+  return {
+    addItem: function(type, des, val) {
+      var newItem, id;
+      id = 0;
+      if (type === "exp") {
+        newItem = new Expense(id, des, val);
+      } else if (type === "inc") {
+        newItem = new Income(id, des, val);
+      }
+    }
+  };
+})();
 
 //UI CONTROLLER
 var UIController = (function() {
@@ -25,25 +57,36 @@ var UIController = (function() {
   };
 })();
 
-// THE APP CONTROLLER
+//THE APP CONTROLLER
 var controller = (function(budgetCtrl, uICtrl) {
-  var dom = uICtrl.getDOMString();
+  //CallBACKFUNC
   var ctrlAddItem = function() {
     //1. Get the field input DATA
     var input = uICtrl.getInput();
 
-    console.log(input);
     //2. Add the item to the budget controller
     //3. Add the item to UI
     //4. Calculate the BUDGET
     //5. Display the BUDGET on the UI
   };
 
-  document.querySelector(dom.inputBtn).addEventListener("click", ctrlAddItem);
+  //setupEventListeners
+  var setupEventListeners = function() {
+    var dom = uICtrl.getDOMString();
+    document.querySelector(dom.inputBtn).addEventListener("click", ctrlAddItem);
+    document.addEventListener("keypress", function(e) {
+      if (e.keyCode === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
 
-  document.addEventListener("keypress", function(e) {
-    if (e.keyCode === 13) {
-      ctrlAddItem();
+  return {
+    init: function() {
+      console.log("Appliction has started");
+      setupEventListeners();
     }
-  });
+  };
 })(budgetController, UIController);
+
+controller.init();
